cad9f87218c2352efcfcfe6dfb9a3c654718a010
wenxue <714876076@qq.com>
2019-05-20 15:59:00 +0800

    no message

--- src/views/history-search/user-message.vue
+++ src/views/history-search/user-message.vue
@@ -1,85 +1,124 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="我的">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
-      </el-form-item>
-    </el-form>
+
+    <div class="block">
+      开始时间：
+      <el-date-picker v-model="createDate" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间" :picker-options="pickerOptionsStart" style="margin-right: 10px;" @change="startTimeStatus" />
+      至
+      <el-date-picker v-model="overDate" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间" :picker-options="pickerOptionsEnd" style="margin-left: 10px;" @change="endTimeStatus" />
+      <span style="margin-left:10px">车牌号</span>
+      <el-select v-model="plateValue" filterable placeholder="请选择车牌号码">
+        <el-option
+          v-for="item in plateList"
+          :key="item.messageId"
+          :label="item.plateNum"
+          :value="item.plateNum"
+        />
+      </el-select>
+
+      <el-button style="margin-left:10px" type="primary" icon="el-icon-search">搜索</el-button>
+    </div>
+
+    <el-table :data="tableList" border fit highlight-current-row style="margin-top:10px">
+      <el-table-column align="center" label="ID" width="50">
+        <template slot-scope="scope">
+          {{ scope.$index+1 }}
+        </template>
+      </el-table-column>
+      <el-table-column label="消息内容" width="300" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.massageContent }}
+        </template>
+      </el-table-column>
+      <el-table-column label="发送用户" width="260" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.driverName}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="发送时间" width="300" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.massageTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="发送车辆" width="260" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.plateNum }} </span>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+  import { getUserMassageFormList } from '@/api/history-search'
+  import { getCarList } from '@/api/vehicle-manage'
+
+  export default {
+
+    filters: {
+
+    },
+    data() {
+      return {
+        list: [],
+        alamTypeValue: [],
+        plateValue: [],
+        plateList: [],
+        createDate: '',
+        overDate: '',
+        pickerOptionsStart: {
+          disabledDate: time => {
+            const endDateVal = this.overDate
+            if (endDateVal) {
+              return time.getTime() > new Date(endDateVal).getTime()
+            }
+          }
+        },
+        pickerOptionsEnd: {
+          disabledDate: time => {
+            const beginDateVal = this.createDate
+            if (beginDateVal) {
+              return (
+                time.getTime() <
+                new Date(beginDateVal).getTime()
+              )
+            }
+          }
+        }
+
+      }
+    },
+    computed: {
+
+      'tableList': function() {
+        return this.list.filter(item => {
+          if (!this.plateValue || item.plateNum === this.plateValue) {
+              if (!this.createDate || !this.overDate) { return true }
+              if (item.massageTime > this.createDate && item.massageTime < this.overDate) { return true }
+
+          } else { return false }
+        })
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      fetchData() {
+        getUserMassageFormList().then(response => {
+          this.list = response.data
+        }),
+          getCarList().then(response => {
+            this.plateList = response.data
+          })
+      },
+      startTimeStatus: function(value) {
+        this.createDate = value
+      },
+      // 时间结束选择器
+      endTimeStatus: function(value) {
+        this.overDate = value
       }
     }
-  },
-  methods: {
-    onSubmit() {
-      this.$message('submit!')
-    },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
-    }
   }
-}
 </script>
-
-<style scoped>
-.line{
-  text-align: center;
-}
-</style>
-
