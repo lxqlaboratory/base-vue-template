1fcbcfc8fd6c5ced66c488f229e47a93cb0525a2
wenxue <714876076@qq.com>
2019-05-21 09:23:18 +0800

    wx

--- src/api/driver-manage.js
+++ src/api/driver-manage.js
@@ -6,3 +6,15 @@ export function getDriverList() {
     method: 'POST'
   })
 }
+
+export function editDriver(driverName, drivingLicenseNum, driverPhoneNum) {
+  return request({
+    url: '/func/web/editDriverList',
+    method: 'POST',
+    data: {
+      driverName,
+      drivingLicenseNum,
+      driverPhoneNum
+    }
+  })
+}
