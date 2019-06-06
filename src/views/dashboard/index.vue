e8b9a10c02f631a1a32a2772fe9a6b2e2e30e079
persue123 <1220438725@qq.com>
2019-06-06 17:51:16 +0800

    ss

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -333,6 +333,75 @@ export default {
       this.center.lng = e.point.lng
       this.center.lat = e.point.lat
     },
+    GpsToBaiduPoint(lat,lng){
+      var _t = this.wgs2bd(lat,lng);
+      /*var _BPoint = new BMap.Point(_t[1], _t[0]);
+      return _BPoint*/
+      return _t
+    },
+    wgs2bd(lat,lon) {
+      var wgs2gcjR = this.wgs2gcj(lat, lon);
+      var gcj2bdR = this.gcj2bd(wgs2gcjR[0], wgs2gcjR[1]);
+      return gcj2bdR
+    },
+    wgs2gcj(lat,lon) {
+      var pi = 3.14159265358979324
+      var a = 6378245.0
+      var ee = 0.00669342162296594323
+      var x_pi = 3.14159265358979324*3000.0/180.0
+      var dLat = this.transformLat(lon - 105.0, lat - 35.0);
+      var dLon = this.transformLon(lon - 105.0, lat - 35.0);
+      var radLat = lat / 180.0 * pi;
+      var magic = Math.sin(radLat);
+      magic = 1 - ee * magic * magic;
+      var sqrtMagic = Math.sqrt(magic);
+      dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+      dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * pi);
+      var mgLat = lat + dLat;
+      var mgLon = lon + dLon;
+      var result = [];
+      result.push(mgLat);
+      result.push(mgLon);
+      return result
+    },
+    transformLat(lat,lon) {
+      var pi = 3.14159265358979324
+      var a = 6378245.0
+      var ee = 0.00669342162296594323
+      var x_pi = 3.14159265358979324*3000.0/180.0
+      var ret = -100.0 + 2.0 * lat + 3.0 * lon + 0.2 * lon * lon + 0.1 * lat * lon + 0.2 * Math.sqrt(Math.abs(lat));
+      ret += (20.0 * Math.sin(6.0 * lat * pi) + 20.0 * Math.sin(2.0 * lat * pi)) * 2.0 / 3.0;
+      ret += (20.0 * Math.sin(lon * pi) + 40.0 * Math.sin(lon / 3.0 * pi)) * 2.0 / 3.0;
+      ret += (160.0 * Math.sin(lon / 12.0 * pi) + 320 * Math.sin(lon * pi  / 30.0)) * 2.0 / 3.0;
+      return ret
+     },
+
+    transformLon(lat,lon) {
+      var pi = 3.14159265358979324
+      var a = 6378245.0
+      var ee = 0.00669342162296594323
+      var x_pi = 3.14159265358979324*3000.0/180.0
+      var ret = 300.0 + lat + 2.0 * lon + 0.1 * lat * lat + 0.1 * lat * lon + 0.1 * Math.sqrt(Math.abs(lat));
+      ret += (20.0 * Math.sin(6.0 * lat * pi) + 20.0 * Math.sin(2.0 * lat * pi)) * 2.0 / 3.0;
+      ret += (20.0 * Math.sin(lat * pi) + 40.0 * Math.sin(lat / 3.0 * pi)) * 2.0 / 3.0;
+      ret += (150.0 * Math.sin(lat / 12.0 * pi) + 300.0 * Math.sin(lat / 30.0 * pi)) * 2.0 / 3.0;
+      return ret
+    },
+    gcj2bd(lat,lon) {
+     var pi = 3.14159265358979324
+     var a = 6378245.0
+     var ee = 0.00669342162296594323
+     var x_pi = 3.14159265358979324*3000.0/180.0
+     var x = lon, y = lat;
+     var z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi);
+     var theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi);
+     var bd_lon = z * Math.cos(theta) + 0.0065;
+     var bd_lat = z * Math.sin(theta) + 0.006;
+     var result = [];
+     result.push(bd_lat);
+     result.push(bd_lon);
+     return result
+    },
     webSocket() {
       const ws = new WebSocket('ws://202.194.14.72:15674/ws')
       var ref = this
@@ -348,8 +417,10 @@ export default {
             if (item.phoneNum == terminalPhone) {
               ref.socketPlateNum = item.plateNum
               // 设置 carList 的值   "latitude" : 36665736, "longitude" : 117132753
-              item.longitude = p.longitude/1000000.0
-              item.latitude = p.latitude/1000000.0
+              var resultPoint = ref.GpsToBaiduPoint(p.latitude/1000000.0,p.longitude/1000000.0);
+              console.log(resultPoint)
+              item.longitude = resultPoint[1]
+              item.latitude = resultPoint[0]
               item.ACC = p.ACC
               item.receiveData=1//代表了已经接收到了信息
               console.log(item.longitude+"--->"+item.latitude)
