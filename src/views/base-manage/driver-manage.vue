219ab272394d0b5a6b51267c32990401957e3d6a
persue123 <1220438725@qq.com>
2019-06-07 10:54:30 +0800

    no message

--- src/views/base-manage/driver-manage.vue
+++ src/views/base-manage/driver-manage.vue
@@ -226,18 +226,6 @@ export default {
     },
     deleteDriver(item) {
       this.insertRow = item
-      /*deleteDriver(this.insertRow.identificationCardNum).then(res => {
-        if(res.re==1){
-          this.$message({
-            message: '删除成功',
-            type: 'success'
-          });
-        }else{
-          this.$message.error('删除失败');
-        }
-      }).catch(e => {
-
-      })*/
       this.$confirm('此操作将永久删除该信息, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
