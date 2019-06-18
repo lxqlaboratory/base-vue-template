0aa1dab08092d6aa76393160aca486091484c4ac
persue123 <1220438725@qq.com>
2019-06-18 16:40:06 +0800

    no message

--- src/utils/timeCompareMinute.js
+++ src/utils/timeCompareMinute.js
@@ -15,4 +15,33 @@ timeCompareMinute.timeCompareMinute= function (time1, time2 ) {
   const minutes = m + n
   return minutes
 };
+
+timeCompareMinute.diffTime= function (startDate, endDate) {
+  let diff=endDate.getTime() - startDate.getTime()//时间差的毫秒数
+  //计算出相差天数
+  let days=Math.floor(diff/(24*3600*1000))
+  //计算出小时数
+  let leave1=diff%(24*3600*1000);    //计算天数后剩余的毫秒数
+  let hours=Math.floor(leave1/(3600*1000))
+  //计算相差分钟数
+  let leave2=leave1%(3600*1000);        //计算小时数后剩余的毫秒数
+  let minutes=Math.floor(leave2/(60*1000))
+  //计算相差秒数
+  let leave3=leave2%(60*1000);      //计算分钟数后剩余的毫秒数
+  let seconds=Math.round(leave3/1000)
+  let returnStr =''
+  if(days>0) {
+    returnStr = returnStr+'day:'+days
+  }
+  if(hours>0) {
+    returnStr =returnStr+'h:'+hours
+  }
+  if(minutes>0) {
+    returnStr = returnStr+'m:'+minutes
+  }
+  if(seconds>0) {
+    returnStr = returnStr + 's:' + seconds
+  }
+  return returnStr
+};
 export default timeCompareMinute
